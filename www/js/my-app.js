d9e1520d929cb5d94f13d607bca777bb7e856ade
Lucas_Diodati <lucasmartindiodati@gmail.com>
2020-04-15 18:36:17 -0300

    Agregado el chat general y el menu lateral.

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -34,6 +34,10 @@ var app = new Framework7({
         path: '/chats/',
         url: 'chats.html',
       },
+      {
+        path: '/chat-general/',
+        url: 'chat-general.html',
+      },
     ]
     // ... other parameters
   });
@@ -43,6 +47,8 @@ var mainView = app.views.create('.view-main');
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+
+var email , password, tituloChat;
 
 });
 
@@ -66,8 +72,13 @@ $$(document).on('page:init', '.page[data-name="inicioSesion"]', function (e) {
 // login con firebase
 
 $$('#ingresar').on('click',function(){
-var email = $$('#emailLogin').val();
-var password = $$('#passwordLogin').val();
+
+// email = $$('#emailLogin').val();
+// password = $$('#passwordLogin').val();
+// Estoy puenteando el login para trabajar más comodo con las vistas
+ email = "programacion21@live.com";
+ password = "kat13579";
+
 //Se declara la variable huboError (bandera)
         var huboError = 0;
 firebase.auth().signInWithEmailAndPassword(email, password)
@@ -210,3 +221,148 @@ console.log("lat "+lat+" lon "+lon);
  /*codigo de la api de mapas, supongo**/
 
 })
+
+/****************************************************************************************/
+$$(document).on('page:init', '.page[data-name="chats"]', function (e) {
+
+$$('#chatGeneral').on('click',function(){tituloChat = "Chat general"; mainView.router.navigate('/chat-general/');});
+$$('#chatCasa').on('click',function(){tituloChat = "Casa"; mainView.router.navigate('/chat-general/'); });
+
+
+
+
+})
+
+/****************************************************************************************/
+$$(document).on('page:init', '.page[data-name="chat-general"]', function (e) {
+
+$$('#tituloChat').text(tituloChat);
+
+// Init Messages
+var messages = app.messages.create({
+  el: '.messages',
+
+  // First message rule
+  firstMessageRule: function (message, previousMessage, nextMessage) {
+    // Skip if title
+    if (message.isTitle) return false;
+    /* if:
+      - there is no previous message
+      - or previous message type (send/received) is different
+      - or previous message sender name is different
+    */
+    if (!previousMessage || previousMessage.type !== message.type || previousMessage.name !== message.name) return true;
+    return false;
+  },
+  // Last message rule
+  lastMessageRule: function (message, previousMessage, nextMessage) {
+    // Skip if title
+    if (message.isTitle) return false;
+    /* if:
+      - there is no next message
+      - or next message type (send/received) is different
+      - or next message sender name is different
+    */
+    if (!nextMessage || nextMessage.type !== message.type || nextMessage.name !== message.name) return true;
+    return false;
+  },
+  // Last message rule
+  tailMessageRule: function (message, previousMessage, nextMessage) {
+    // Skip if title
+    if (message.isTitle) return false;
+      /* if (bascially same as lastMessageRule):
+      - there is no next message
+      - or next message type (send/received) is different
+      - or next message sender name is different
+    */
+    if (!nextMessage || nextMessage.type !== message.type || nextMessage.name !== message.name) return true;
+    return false;
+  }
+});
+
+// Init Messagebar
+var messagebar = app.messagebar.create({
+  el: '.messagebar'
+});
+
+// Response flag
+var responseInProgress = false;
+
+// Send Message
+$$('.send-link').on('click', function () {
+  var text = messagebar.getValue().replace(/\n/g, '<br>').trim();
+  // return if empty message
+  if (!text.length) return;
+
+  // Clear area
+  messagebar.clear();
+
+  // Return focus to area
+  messagebar.focus();
+
+  // Add message to messages
+  messages.addMessage({
+    text: text,
+  });
+
+  if (responseInProgress) return;
+  // Receive dummy message
+  receiveMessage();
+});
+
+// Dummy response
+var answers = [
+  'Yes!',
+  'No',
+  'Hm...',
+  'I am not sure',
+  'And what about you?',
+  'May be ;)',
+  'Lorem ipsum dolor sit amet, consectetur',
+  'What?',
+  'Are you sure?',
+  'Of course',
+  'Need to think about it',
+  'Amazing!!!'
+]
+var people = [
+  {
+    name: 'Kate Johnson',
+    avatar: 'https://cdn.framework7.io/placeholder/people-100x100-9.jpg'
+  },
+  {
+    name: 'Blue Ninja',
+    avatar: 'https://cdn.framework7.io/placeholder/people-100x100-7.jpg'
+  }
+];
+function receiveMessage() {
+  responseInProgress = true;
+  setTimeout(function () {
+    // Get random answer and random person
+    var answer = answers[Math.floor(Math.random() * answers.length)];
+    var person = people[Math.floor(Math.random() * people.length)];
+
+    // Show typing indicator
+    messages.showTyping({
+      header: person.name + ' is typing',
+      avatar: person.avatar
+    });
+
+    setTimeout(function () {
+      // Add received dummy message
+      messages.addMessage({
+        text: answer,
+        type: 'received',
+        name: person.name,
+        avatar: person.avatar
+      });
+      // Hide typing indicator
+      messages.hideTyping();
+      responseInProgress = false;
+    }, 4000);
+  }, 1000);
+}
+
+
+})
+
